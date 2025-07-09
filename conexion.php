45a9112e2113f022a64d7ce1f138519b085a04a2
roman <romanquiroga456@gmail.com>
2025-07-09 01:34:40 -0300

    corregimos el archivo conexion

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "tienda");
+$conexion = new mysqli("sql312.infinityfree.com", "if0_39426784", "locuritas3000", "if0_39426784_tienda");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
